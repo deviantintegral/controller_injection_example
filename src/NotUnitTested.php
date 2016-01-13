658dc2d8cae2434d6a78ae85948c12250dbf3a12
Andrew Berry <deviantintegral@gmail.com>
2016-01-12 20:36:57 -0500

    Doc calling __construct() directly

--- src/NotUnitTested.php
+++ src/NotUnitTested.php
@@ -7,6 +7,10 @@ class NotUnitTested {
   public function render() {
     // The only thing a caller needs to do is to know to call create().
     $controller = HelloWorldController::create(\Drupal::getContainer());
+
+    // If a caller only wanted to use \Drupal for a part of a call, they could
+    // call __construct() directly:
+    // $controller = new HelloWorldController($configObject, \Drupal::translation());
     $response = $controller->render();
 
     $response['#markup'] = 'This is totally not unit tested.';
